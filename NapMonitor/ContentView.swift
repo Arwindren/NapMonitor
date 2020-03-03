688dcdfbd9854f4b71c2a98dcd39fc379a64d83a
Arwindren <arwindren@gmail.com>
2020-03-03 15:14:18 +0100

    Initial Commit

---
+++ NapMonitor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NapMonitor
+//
+//  Created by Arwin Krishnan on 03/03/2020.
+//  Copyright © 2020 Arwin Krishnan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
